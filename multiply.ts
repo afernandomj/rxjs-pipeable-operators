0a9c27722364505fd43341aaecc4b122bb79d378
Andres Moreno <andres.moreno@pluto.tv>
2023-06-25 16:18:40 -0500

    create operators from existing operators

--- multiply.ts
+++ multiply.ts
@@ -1,21 +1,4 @@
-import { Observable, Subscriber } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-class MultiplySubscriber extends Subscriber<number> {
-	constructor(sub: Subscriber<number>, private number: number) {
-		super(sub);
-		this.number = number;
-	}
-	_next(value: number): void {
-		console.log('this.number', this.number);
-		console.log('MultiplySubscriber', value);
-		this.destination.next && this.destination.next(value * this.number);
-	}
-}
-
-export const multiply = (number: number) => (source: Observable<number>) => {
-	return source.lift({
-		call(sub: Subscriber<number>, source: Observable<number>) {
-			source.subscribe(new MultiplySubscriber(sub, number));
-		},
-	});
-};
+export const multiply = (number: number) =>
+	map((value: number) => value * number);
