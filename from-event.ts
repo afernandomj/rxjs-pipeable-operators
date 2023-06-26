c2b960bb199a472edd9610d6939ca30ca8ba38dd
Andres Moreno <andres.moreno@pluto.tv>
2023-06-25 23:07:25 -0500

    switchMap by canceling inner subscriptions values

--- from-event.ts
+++ from-event.ts
@@ -1,7 +1,9 @@
 import { Observable, Subscriber, fromEvent, of } from 'rxjs';
 import { delay, mergeMap, scan } from 'rxjs/operators';
 
-class MyMergeMapSubscriber extends Subscriber<any> {
+class MySwitchMapSubscriber extends Subscriber<any> {
+	innerSubscription: any;
+
 	constructor(sub: Subscriber<any>, private fn: any) {
 		super(sub);
 		this.fn = fn;
@@ -11,7 +13,14 @@ class MyMergeMapSubscriber extends Subscriber<any> {
 		console.log(`outer ${value}`);
 		const o$ = this.fn(value);
 
-		o$.subscribe({
+		// removing this, turn this class into a mergemap
+		if (this.innerSubscription) {
+			// this will cancel previous inner values called,
+			// and only shows the last inner value that was called.
+			this.innerSubscription.unsubscribe();
+		}
+
+		this.innerSubscription = o$.subscribe({
 			next: (value: any) => {
 				console.log(`inner ${value}`);
 				this.destination.next && this.destination.next(value);
@@ -20,17 +29,17 @@ class MyMergeMapSubscriber extends Subscriber<any> {
 	}
 }
 
-const myMergeMap = (fn: any) => (source: Observable<any>) =>
+const mySwitchMap = (fn: any) => (source: Observable<any>) =>
 	source.lift({
 		call(sub, source) {
-			source.subscribe(new MyMergeMapSubscriber(sub, fn));
+			source.subscribe(new MySwitchMapSubscriber(sub, fn));
 		},
 	});
 
 const observable$ = fromEvent(document, 'click').pipe(
 	scan((i) => i + 1, 0),
 	// mergeMap((value) => of(value).pipe(delay(500)))
-	myMergeMap((value: any) => of(value).pipe(delay(500)))
+	mySwitchMap((value: any) => of(value).pipe(delay(500)))
 );
 
 const subscriber = {
